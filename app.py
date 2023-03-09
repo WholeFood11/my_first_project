2a01157bba6b0194246892c3f7354f5ae0b1ad0d
WholeFood11 <nehaljain7@deloitte.com>
2023-03-09 15:21:40 +0530

    test

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+from my_first_project.my_first_project_stack import MyFirstProjectStack
+
+
+app = cdk.App()
+MyFirstProjectStack(app, "WFM-S3",
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    env=cdk.Environment(account='054548226963', region='us-east-2'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
+
+MyFirstProjectStack(app, "MyPipelineStack", 
+    env=cdk.Environment(account='054548226963', region='us-east-2')
+)
+
+
+app.synth()
---
+++ my_first_project/my_first_project_stack.py
@@ -0,0 +1,24 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+from aws_cdk import cloudformation_include as cfn_inc
+from constructs import Construct
+#from aws_cdk import Stack
+
+class MyFirstProjectStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        template = cfn_inc.CfnInclude(self, "Template",  
+            template_file="migration_template.yaml")
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "MyFirstProjectQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
