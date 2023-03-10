5517fb77f383a84e4fdc676a7ca0452914e0459d
WholeFood11 <nehaljain7@deloitte.com>
2023-03-10 13:57:19 +0530

    updated

--- my_first_project/my_pipeline_stack.py
+++ my_first_project/my_pipeline_stack.py
@@ -31,7 +31,6 @@ class MyPipelineStack(cdk.Stack):
         europe_wave.add_stage(MyPipelineAppStage(self, "Ireland",
             env=cdk.Environment(account='054548226963',region="eu-west-1")
         ))
-        
         europe_wave.add_stage(MyPipelineAppStage(self, "Paris",
             env=cdk.Environment(account='054548226963',region="eu-west-3")
         ))
